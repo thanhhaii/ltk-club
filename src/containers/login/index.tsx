eef44c3ca2272bc027be3fecab1a4db13c4ebe03
thanhhaii <hailamnguyenthanh@gmail.com>
2022-09-25 22:02:45 +0700

    develop static login page

--- src/containers/login/index.tsx
+++ src/containers/login/index.tsx
@@ -1,35 +1,72 @@
 import React from 'react';
-import { Input, Form, Row, Col } from "antd"
+import { Input, Form, Row, Col, Button } from "antd"
 import Image from 'next/image'
-import FormInput from "../../components/formComponents/input"
+import FormInput from "@components/formComponents/input"
 import IconClub from "public/images/logocaulacbo.png";
+import { LoginOutlined } from '@ant-design/icons';
+import { LoginFormProps } from '@models/form/login';
+
 
 export type LoginPageProps = {
 
 };
 
 const LoginPageContainer: React.FC<LoginPageProps> = () => {
-  const [form] = Form.useForm();
+  const [form] = Form.useForm<LoginFormProps>();
 
   return (
     <main className="container mx-auto h-screen flex items-center justify-center">
-      <div className="shadow-lg bg-white rounded-lg p-5">
+      <div className="shadow-lg bg-white rounded-3xl p-10 w-5/6 sm:w-2/3 md:3/5 lg:w-2/5 xl:w-1/3">
         <Form form={form} layout="vertical">
-          <Row gutter={[24,0]}>
+          <Row gutter={[24, 0]}>
             <Col span={24} className="text-center">
-              <div className="relative">
-                <Image src={IconClub} alt="logoCLB" width={100} height={100}/>
+              <div className="relative mb-3">
+                <Image src={IconClub} alt="logoCLB" width={70} height={70} />
               </div>
+              <h1 className="text-2xl uppercase">Đăng nhập</h1>
             </Col>
             <Col span={24}>
-              <Form.Item label="Email" required>
-                <FormInput size="large" />
+              <Form.Item
+                name="username"
+                label="Tên tài khoản"
+                rules={[
+                  { required: true, message: 'Tên tài khoản không được để trống' }
+                ]}
+                required>
+                <FormInput
+                  size="large"
+                  placeholder="Số điện thoại / Email"
+                />
               </Form.Item>
             </Col>
             <Col span={24}>
-              <Form.Item label="Password" required>
-                <Input.Password size="large" />
+              <Form.Item
+                name="password"
+                label="Mật khẩu"
+                required
+                rules={[
+                  { required: true, message: 'Mật khẩu không được để trống' }
+                ]}
+              >
+                <Input.Password size="large" className="!rounded-md" placeholder="Mật khẩu" />
               </Form.Item>
+            </Col>
+            <Col span={24} className="pt-2">
+              <Button
+                type="primary"
+                className="w-full !rounded-md"
+                icon={<LoginOutlined />}
+                htmlType="submit"
+                size="large">
+                Đăng nhập
+              </Button>
+            </Col>
+            <Col span={24}>
+              <div className="mt-4">
+                <p>Bạn chưa có tài khoản?&nbsp;
+                  <span className="font-medium text-blue-500 cursor-pointer">Liên hệ Admin!</span>
+                </p>
+              </div>
             </Col>
           </Row>
         </Form>
---
+++ src/models/form/login.ts
@@ -0,0 +1,4 @@
+export type LoginFormProps = {
+    username: string
+    password: string
+}
